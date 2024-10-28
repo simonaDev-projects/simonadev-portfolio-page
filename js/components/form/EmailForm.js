7e0238d047284fe9967916f0520c5cd6eec98303
Simona <simonasablinaite@gmail.com>
2024-10-28 21:16:52 +0200

    Input validations are displayed in the console

--- js/components/form/EmailForm.js
+++ js/components/form/EmailForm.js
@@ -64,11 +64,80 @@ class EmailForm {
    }
 
    addEventListeners() {
+      /*
+      EIGA:
+      1. Reikia susirasti atitinkamus laukus, norint juos validuoti;
+      2. Issitraukti, kas juose parasyta;
+      3. Patikrinti, kas parasyta, ar duomenys teisingi
+      4. Paspaudus mygtuka, atitinkamai kas buvo ivesta, pateikti forma arba gauti klaidos pranesima
+      Paspaudus mygtuka 'submit' reikia patikrinti, ar visi laukai uzpildyti teisingai
+      */
+
       console.log(this.FORM);
       const fields = this.FORM.querySelectorAll('.item');
+      const submitDOM = document.createElement('button');
+      submitDOM.classList.add('btn');
+      submitDOM.innerText = "Send message";
+
+      console.log(fields[0].value);
+
+      const isValid = {
+         string: (str) => typeof str === 'string' && str.trim() !== '',
+         minSize: (str, size) => str.length >= size,
+         maxSize: (str, size) => str.length <= size,
+         username: (str) => isValid.string(str) && isValid.minSize(str, 3) && isValid.maxSize(str, 20),
+         email: (str) => isValid.string(str) && isValid.minSize(str, 6) && isValid.maxSize(str, 50) && str.includes('@'),
+         phone: (str) => isValid.string(str),
+         subject: (str) => isValid.string(str) && isValid.minSize(str, 10) && isValid.maxSize(str, 50),
+         message: (str) => isValid.string(str),
+      }
+
+
+      if (submitDOM) {
+         function submitForm(e) {
+            e.preventDefault();
+
+            const usernameValid = isValid.username(fields[0].value);
+            const emailValid = isValid.email(fields[1].value);
+            const phoneValid = isValid.phone(fields[2].value);
+            const subjectValid = isValid.subject(fields[3].value);
+            const messageValid = isValid.message(fields[4].value);
+
+            if (!usernameValid) {
+               console.log('Blogas username');
+            }
+
+            if (!emailValid) {
+               console.log('Blogas email adresas');
+            }
+
+            if (!phoneValid) {
+               console.log('Blogas telefono numeris');
+            }
+
+            if (!subjectValid) {
+               console.log('Negeras pavadinimas');
+            }
+
+            if (!messageValid) {
+               console.log('Zinuteje yra klaidu');
+            }
+
+            if (usernameValid && emailValid && phoneValid && subjectValid && messageValid) {
+               fields.classList.add('correctly')
+            }
+         }
+
+         submitDOM.addEventListener("click", submitForm)
+      }
+
+
+
 
       // Gaunami visi formos elementai. Dabar reikia suvaliduoti kiekviena atskirai
-      console.log(fields);
+
+
+      this.FORM.appendChild(submitDOM);
    }
 }
 
